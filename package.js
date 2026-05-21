e94b97c3eaafc8f2a6a59cbb3583a9987fa3b742
ulriklarsen50-debug <ulriklarsen50@gmail.com>
2026-05-21 07:01:51 +0200

    Initialize package.json with project details

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "lifa-forsyning",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.0",
+    "cors": "^2.8.5"
+  }
+}
+
