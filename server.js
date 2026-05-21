240cd44388f405c34974c0dd1a276e44bb2f41c1
ulriklarsen50-debug <ulriklarsen50@gmail.com>
2026-05-21 07:13:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const cors    = require('cors');
 const { generate } = require('./generateData');
 
 const app  = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
